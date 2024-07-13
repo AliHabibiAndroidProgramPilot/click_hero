45a145535f8ebbfb209f3b4df3cc4b84b71cfabd
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-13 15:47:04 +0330

    click Functional

--- app/src/main/java/com/click_hero/ali/MainActivity.kt
+++ app/src/main/java/com/click_hero/ali/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.click_hero.ali
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.click_hero.ali.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        core()
     }
+    private var counter: Int = 0
+    private fun core() {
+        binding.btnClick.setOnClickListener {
+            counter++
+            binding.progressHorizontal.progress = counter
+            binding.txtNumberCounter.text = counter.toString()
+        }
+    }
+
 }
