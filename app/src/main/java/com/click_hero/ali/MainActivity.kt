6109a0eaa3635fa101f436f44382f1cba22cb5d6
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-14 15:43:24 +0330

    making app

--- app/src/main/java/com/click_hero/ali/MainActivity.kt
+++ app/src/main/java/com/click_hero/ali/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.click_hero.ali
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.click_hero.ali.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var preferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        core()
-    }
-    private var counter: Int = 0
-    private fun core() {
+        preferences = getPreferences(MODE_PRIVATE)
+        val editor = preferences.edit()
+        notifierViews(preferences.getInt("counter", 0))
         binding.btnClick.setOnClickListener {
-            counter++
-            binding.progressHorizontal.progress = counter
-            binding.txtNumberCounter.text = counter.toString()
+            editor.putInt("counter", binding.txtNumberCounter.text.toString().toInt() + 1).apply()
+            notifierViews(preferences.getInt("counter", 0))
         }
     }
 
+    private fun notifierViews(notif: Int) {
+        binding.txtNumberCounter.text = notif.toString()
+        binding.progressHorizontal.progress = notif
+    }
 }
